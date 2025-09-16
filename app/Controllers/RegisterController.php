5faf4387dfb4f65ed749bd5deadaf3c6c2f5dfd9
KevinVigne <kev.vigne@gmail.com>
2025-09-16 15:13:29 +0200

    Récupérer et Vérifier les données

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -1,3 +1,79 @@
 <?php
-    require_once (__DIR__ .'/../Views/register.view.php');
 
+function isNotEmpty ($value){
+    //Création d'un tableau vide
+    global $arrayError;
+
+    //si le post avec la valeur est vide alors
+    if(empty($_POST[$value])){
+        //On rapelle le tableau et on lui donne en clé le nom de la $value et en valeur une string
+        $arrayError[$value] = "Le champs $value ne peut pas être vide.";
+        //On retour le tableau
+        return $arrayError;
+    }
+
+    //sinon on retourne false
+    return false;
+}
+
+function checkFormat($nameInput,$value){
+    //Création d'un tableau vide
+    global $arrayError;
+
+     //Vos regex = vos filtres
+    $regexPseudo = '/^([0-9a-z_\-.A-Zà-üÀ-Ü]){3,255}$/';
+    $regexPassword = '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/';
+
+    //on prend le nom de l'input
+    switch($nameInput){
+
+        //Si l'input s'appel pseudo alors 
+        case 'pseudo':
+
+            //si la valeur de l'input n'arrive pas a passer le filtre alors
+            if(!preg_match($regexPseudo, $value)){
+                //on appel notre tableau et on ajoute en clé pseudo et en valeur la string
+                $arrayError['pseudo'] = 'Merci de renseigner un pseudo correcte!';
+            }
+            break;
+        case 'password':
+            if(!preg_match($regexPassword,$value)){
+                $arrayError['password'] = 'Veuillez renseigner un mot de passe valide ( 8 caractères minimum, 1 Majuscule, 1 Chiffre , 1 caractère spécial  et 1 lettre minuscule )';
+            }
+            break;
+        case 'email':
+            // Vérifie si l'email est valide
+            if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
+                $arrayError['email'] = 'Veuillez entrer un email valide ';
+            }
+            break;
+    }
+}
+
+
+
+if(isset($_POST['pseudo'])){
+    $valueEmail = htmlspecialchars($_POST['email']);
+    $valuePassword = htmlspecialchars($_POST['password']);
+    $valuePseudo = htmlspecialchars($_POST['pseudo']);
+
+    checkFormat('pseudo',$valuePseudo);
+    checkFormat('password',$valuePassword);
+    checkFormat('email',$valueEmail);
+
+    isNotEmpty('pseudo');
+    isNotEmpty('email');
+    isNotEmpty('password');
+
+    // nos erreurs seront affichés dans :
+    var_dump($arrayError);
+
+
+
+    require_once( __DIR__ . "/../Views/register.view.php" );
+}else{
+    
+    require_once( __DIR__ . "/../Views/register.view.php" );
+}
+    
+ 
--- app/Views/register.view.php
+++ app/Views/register.view.php
@@ -3,13 +3,44 @@
 ?>
 <h1 class ="d-flex justify-content-center text-warning">Inscription</h1>
 <form method = "post">
-    <label for="pseudo">Nom d'utilisateur :</label>
-    <input type="text" placeholder = "Rapido&Razmodu78" name = "pseudo" >
-    <label for="password">Mot de Passe :</label>
-    <input type="password" placeholder = "mot de passe" name = "password" >
-    <label for="email"> Adresse Mail :</label>
-    <input type="email" placeholder ="exemple@gmail.com" name = "email" >
-    <button type = "submit">S'inscrire</button>
+
+    <div class="container">
+        <div class="form-group">
+            <label for="pseudo" class="form-label">Nom d'utilisateur :</label>
+            <input type="text" name="pseudo" id="pseudo" placeholder="Toto" class="form-control">
+            <?php 
+            if(isset($arrayError['pseudo'])){
+                ?>
+                    <p class="text-danger"><?= $arrayError['pseudo']?></p>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="form-group">
+            <label for="email" class="form-label">Adresse Mail :</label>
+            <input type="email" name="email" id="email" placeholder="Toto@gmail.com" class="form-control">
+            <?php 
+            if(isset($arrayError['email'])){
+                ?>
+                    <p class="text-danger"><?= $arrayError['email']?></p>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="form-group">
+            <label for="password" class="form-label ">Mot de Passe :</label>
+            <input type="password" name="password" id="password" class="form-control">
+            <?php 
+            if(isset($arrayError['password'])){
+                ?>
+                    <p class="text-danger"><?= $arrayError['password']?></p>
+                <?php
+            }
+            ?>
+        </div>
+        <button type="submit" class="btn btn-success mt-5">Inscription</button>
+    </div>
+
 </form>
 
 
